58def0c0d41e124631bf20cf96070bbdeb7c4f9c
rajkhare <rajkhare78946@gmail.com>
2026-05-10 00:20:12 +0530

    ready for deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: "https://ai-interview-platform-rose-psi.vercel.app/",
     credentials: true,
   }),
 );
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -84,7 +84,11 @@ async function loginUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
   res.status(200).json({
     message: "User loggedIn successfully.",
     user: {
