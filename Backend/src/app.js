b07f4befdf2ddb3b374ddf380b64eb20c52a99a1
rajkhare <rajkhare78946@gmail.com>
2026-05-10 00:24:19 +0530

    fixing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://ai-interview-platform-rose-psi.vercel.app/",
+    origin: "https://ai-interview-platform-rose-psi.vercel.app",
     credentials: true,
   }),
 );
